fc2b3e72b64cf4e5e1b96ce40baa3b30c9898a3c
Charles Yuan <charlesyuan59@gmail.com>
2022-06-23 19:53:06 -0400

    Save results

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <direct.h>
+#include <sys/stat.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
@@ -11,7 +14,9 @@ Mat image, gray, blurred, edge, laplaceEdgeResized, cannyEdgeResized, combined;
 int main()
 {
     // Set the image path to the absolute path of whatever image you want
-    char imagepath[] = "C:/Users/charl/Desktop/edge-art-generator/arknights.png";
+    std::string basedir = "C:/Users/charl/Desktop/edge-art-generator/";
+    std::string imagedir = "arknights.png";
+    std::string imagepath = basedir + imagedir;
 
     // Pass in absolute path just cuz of how CMake runs
     image = imread(imagepath);
@@ -30,13 +35,23 @@ int main()
     // Get Canny edge
     Mat cannyEdge = cannyThreshold(gray, blurred, edge);
 
-    // Resize edge detections
+    // Create folder to store results
+    char _savedir[] = "C:/Users/charl/Desktop/edge-art-generator/results/"; // Must use char, not std::string
+    _mkdir(_savedir);
+    std::string savedir = "results/"; // Converting is annoying
+
+    // Save results
+    imwrite(basedir + savedir + "laplace_edges.png", laplaceEdge);
+    imwrite(basedir + savedir + "canny_edges.png", cannyEdge);
+
+    // Resize edge detections for display
     resize(laplaceEdge, laplaceEdgeResized, Size(), 0.25, 0.25);
     resize(cannyEdge, cannyEdgeResized, Size(), 0.25, 0.25);
 
     // Display windows
     imshow("Laplace Edge Detection", laplaceEdgeResized);
     imshow("Canny Edge Detection", cannyEdgeResized);
+
     waitKey(0);
 
     return 0;
